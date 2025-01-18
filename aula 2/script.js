60ec82bf9855bc62a0995e9933117175f0fa4a1d
Thamires Santos <106447459+thamiresantos@users.noreply.github.com>
2025-01-17 22:53:29 -0300

    aula 03
    corrigindo estruturas condicionais e alterando a cor do texto conforme a mensagem

--- aula 2/script.js
+++ aula 2/script.js
@@ -1,26 +1,31 @@
 function adicionarTarefa() {
-    let mensagem = "Tarefa adicionada com sucesso!"
     
+    //recebe valor do input do usuário
     let inputTarefa = document.getElementById("inputTarefa");
-    let Tarefa = inputTarefa.value;
+    let Tarefa = inputTarefa.value.trim() //trim tira os espaços vazios do começo e do fim da frase.
+    // campo vazio <p>
+   let mensagem = document.getElementById("mensagem")
     
-
-   
-
+    // Se o valor do input estiver vazio, retorna a mensagem:
     if (Tarefa == "") {
-        listaTarefas.textContent = "Campo vazio! Digite algo!"
-    } else {
-        let listaTarefas = document.getElementById("listaTarefas");
-        let novaTarefa = document.createElement('li');
-        
-        novaTarefa.textContent = Tarefa;
-    
-        listaTarefas.appendChild(novaTarefa);
+        mensagem.style.color = '#A34743'
+        mensagem.textContent = "Campo vazio! Digite uma tarefa para adicioná-la a sua!"
 
-        document.getElementById("mensagem").textContent = mensagem;
+    } else {
+        //cria novo item (li) e insere na lista (ul)
+        const listaTarefas = document.getElementById("listaTarefas");
+        let novaTarefa = document.createElement('li');
+        novaTarefa.textContent = Tarefa;
+        listaTarefas.appendChild(novaTarefa); //adicionado ao pai
+        // mensagem de tarerfa adicionada com sucesso
+        mensagem.style.color = '#28A745'
+        mensagem.textContent = "Tarefa adicionada com sucesso!"
+       
     }
 
-    
+    //limpa o input do usuário
+        inputTarefa.value = ""
+ 
 }
 
 
