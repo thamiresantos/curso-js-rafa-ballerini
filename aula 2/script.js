711281ff057de6386966dcd9de189797ad02b91d
Thamires Santos <106447459+thamiresantos@users.noreply.github.com>
2025-02-03 22:47:55 -0300

    aula 04
    criando arrays (listas) e estruturas de repetição (for)

--- aula 2/script.js
+++ aula 2/script.js
@@ -1,3 +1,5 @@
+let tarefas = []
+
 function adicionarTarefa() {
     
     //recebe valor do input do usuário
@@ -9,23 +11,33 @@ function adicionarTarefa() {
     // Se o valor do input estiver vazio, retorna a mensagem:
     if (Tarefa == "") {
         mensagem.style.color = '#A34743'
-        mensagem.textContent = "Campo vazio! Digite uma tarefa para adicioná-la a sua!"
+        mensagem.textContent = "Campo vazio! Digite uma tarefa para adicioná-la a sua lista!"
 
     } else {
-        //cria novo item (li) e insere na lista (ul)
-        const listaTarefas = document.getElementById("listaTarefas");
-        let novaTarefa = document.createElement('li');
-        novaTarefa.textContent = Tarefa;
-        listaTarefas.appendChild(novaTarefa); //adicionado ao pai
         // mensagem de tarerfa adicionada com sucesso
         mensagem.style.color = '#28A745'
         mensagem.textContent = "Tarefa adicionada com sucesso!"
+        tarefas.push(Tarefa)
+        renderizarTarefas()
        
     }
 
     //limpa o input do usuário
         inputTarefa.value = ""
  
+}
+
+function renderizarTarefas() {
+ //cria novo item (li) e insere na lista (ul)
+    const listaTarefas = document.getElementById("listaTarefas");
+    listaTarefas.innerHTML = ""
+
+    let i =0 
+    for (i; i < tarefas.length; i++) {
+        let novaTarefa = document.createElement('li');
+        novaTarefa.textContent = tarefas[i]
+        listaTarefas.appendChild(novaTarefa); //adicionado ao pai
+     }
 }
 
 
